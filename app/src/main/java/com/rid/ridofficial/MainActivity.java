b05c06dd86a7b2e800028da2f79f3f8aecf03b1a
mukul <mukulraw199517@gmail.com>
2019-06-01 20:04:42 +0530

    started

--- app/src/main/java/com/rid/ridofficial/MainActivity.java
+++ app/src/main/java/com/rid/ridofficial/MainActivity.java
@@ -1,13 +1,40 @@
 package com.rid.ridofficial;
 
+import android.graphics.Color;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+
+        drawer = findViewById(R.id.container);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+
+        toolbar.setTitle("RID Official");
+        //toolbar.setSubtitle("red is dead");
+
+        toolbar.setSubtitleTextColor(Color.WHITE);
+
     }
 }
--- app/src/main/java/com/rid/ridofficial/UpdateProfile.java
+++ app/src/main/java/com/rid/ridofficial/UpdateProfile.java
@@ -1,13 +1,32 @@
 package com.rid.ridofficial;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UpdateProfile extends AppCompatActivity {
+
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_profile);
+
+        submit = findViewById(R.id.button3);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(UpdateProfile.this , MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
     }
 }
